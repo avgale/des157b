bbfd4a592a4e9f4115f81365470260a7307f1440
Amabel Gale <avgale@ucdavis.edu>
2025-04-21 13:24:19 -0700

    Bar transitions working. Need to manipulate popup visibility.

--- studio2/script.js
+++ studio2/script.js
@@ -1,4 +1,36 @@
 (function() {
     'use strict';
 
+    const slider = document.querySelector('#slider');
+    let data;
+
+    async function getData(){
+        const raw = await fetch ('data/pullups.json');
+        const jsonData = await raw.json();
+        data = jsonData;
+
+        slider.addEventListener('input', sliderInteraction);
+    }
+
+    getData();
+
+    function sliderInteraction() {
+        let currVal = this.value;
+        let id;
+        let currBar;
+        let monthData;
+        for (const year in data["year"]) {
+            for (const month in data["year"][`${year}`]) {
+                monthData = data["year"][`${year}`][`${month}`];
+                id = `${month}${year}`;
+                currBar = document.querySelector(`#${id}`);
+                if (monthData["id"] < currVal) {
+                    currBar.style.height = `${monthData["value"] * 28}px`;
+                }
+                if (monthData["id"] > currVal) {
+                    currBar.style.height = '0px';
+                }
+            }
+        }
+    }
 })();
