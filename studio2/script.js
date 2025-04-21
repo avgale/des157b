bb45b7004e89fc529df9c3677b624c8a0e89e228
Amabel Gale <avgale@ucdavis.edu>
2025-04-21 13:36:47 -0700

    Added popups. Need to update subheading stats

--- studio2/script.js
+++ studio2/script.js
@@ -2,6 +2,8 @@
     'use strict';
 
     const slider = document.querySelector('#slider');
+    const liftPopup = document.querySelector('#lift');
+    const climbPopup = document.querySelector('#climb');
     let data;
 
     async function getData(){
@@ -19,15 +21,29 @@
         let id;
         let currBar;
         let monthData;
+
+        if (currVal == 4) { // June 2023 id + 1. currVal starts at 1 indexing, month ids start at 0.
+            liftPopup.style.visibility = "visible";
+        } /*else {
+            liftPopup.style.visibility = "hidden";
+        }*/
+
+        if (currVal == 10) { // January 2024 id + 1.
+            climbPopup.style.visibility = "visible";
+        } /*else {
+            climbPopup.style.visibility = "hidden";
+        }*/
+
         for (const year in data["year"]) {
             for (const month in data["year"][`${year}`]) {
                 monthData = data["year"][`${year}`][`${month}`];
                 id = `${month}${year}`;
                 currBar = document.querySelector(`#${id}`);
+
                 if (monthData["id"] < currVal) {
                     currBar.style.height = `${monthData["value"] * 28}px`;
                 }
-                if (monthData["id"] > currVal) {
+                if (monthData["id"] >= currVal) {
                     currBar.style.height = '0px';
                 }
             }
