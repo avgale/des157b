da0019d6e1bad1b6a2a65cbed9aaa031ab67f5fd
avgale <amabel@amabelgale.com>
2025-04-29 11:04:03 -0700

    Fixed friends list

--- friends-list/script.js
+++ friends-list/script.js
@@ -19,13 +19,20 @@
         event.preventDefault();
         addFriendForm.className = "add-friend-offscreen";
     });
+
+    document.addEventListener('click', function(event){
+        console.log(event.target.classList.contains);
+        if (event.target.classList.contains('fa-edit')) {
+            editFriendForm.className = "edit-friend-onscreen";
+        }
+    });
     
-    for (let i = 0; i < editBtns.length; i++) {
+    /*for (let i = 0; i < editBtns.length; i++) {
         editBtns[i].addEventListener("click", function(event){
             event.preventDefault();
             editFriendForm.className = "edit-friend-onscreen";
         });
-    };
+    };*/
     
     editFriendForm.addEventListener("submit", function(event){
         event.preventDefault();
@@ -65,7 +72,7 @@
                     <a href="${linkedin}"><i class="fab fa-linkedin"></i></a>
                 </div>
                 <i class="fas fa-edit" id="e-${id}"></i>
-                <i class=fas fa-times-circle" id="d-${id}></i> `;
+                <i class="fas fa-times-circle" id="d-${id}"></i> `;
     
                 friendList.append(theListItem);
             });
