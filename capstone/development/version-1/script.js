c27a4c76883696fcdfde12851c1c9c6a8b728fd9
Amabel Gale <avgale@ucdavis.edu>
2025-05-22 19:30:09 -0700

    Added hover section

--- capstone/development/version-1/script.js
+++ capstone/development/version-1/script.js
@@ -26,7 +26,7 @@
                 scrollTrigger: {
                     trigger: ".scroll-stage",
                     start: "top top",
-                    end: "+=400%",
+                    end: "+=900%",
                     scrub: true,
                     pin: true,
                 }
@@ -64,9 +64,12 @@
 
                 .fromTo("#stage8", { opacity: 0 }, { opacity: 1, duration: 0.5 })
                 .add(() => splitAnimate("#stage8 p"), "-=0.5")
+                .to("#stage8", { opacity: 0, duration: 0.5 })
+
+                .fromTo("#stage9", { opacity: 0 }, { opacity: 1, duration: 0.5 })
+                .add(() => hoverEvents())
 
             // Handle interactive slider
-
             const slider = document.querySelector("#slider");
             const sliderValue = document.querySelector("#slider-value");
             const greenDot = document.querySelector("#green-dot");
@@ -92,6 +95,65 @@
                 greenDot.style.opacity = "1";
                 greenDotLabel.style.opacity = "1";
             });
+
+            // Hover Events
+            function hoverEvents() {
+                const stage9 = document.querySelector("#stage9");
+                stage9.style.zIndex = "1";
+
+                ScrollTrigger.create({
+                    trigger: "#stage9",
+                    start: "top 80%",
+                    once: true,
+                    onEnter: () => {
+                        // Show all cutouts for a second before activating hover effects
+                        setTimeout(() => {
+                            const iceCream = document.querySelector("#ice-cream");
+                            const iceCreamCaption = document.querySelector("#ice-cream-caption");
+                            const yogurt = document.querySelector("#yogurt");
+                            const yogurtCaption = document.querySelector("#yogurt-caption");
+                            const milk = document.querySelector("#milk");
+                            const milkCaption = document.querySelector("#milk-caption");
+
+                            iceCream.addEventListener("mouseover", function () {
+                                iceCreamCaption.style.opacity = "1";
+                                yogurt.style.opacity = "0";
+                                milk.style.opacity = "0";
+                            });
+
+                            iceCream.addEventListener("mouseout", function () {
+                                iceCreamCaption.style.opacity = "0";
+                                yogurt.style.opacity = "1";
+                                milk.style.opacity = "1";
+                            });
+
+                            yogurt.addEventListener("mouseover", function () {
+                                yogurtCaption.style.opacity = "1";
+                                iceCream.style.opacity = "0";
+                                milk.style.opacity = "0";
+                            });
+
+                            yogurt.addEventListener("mouseout", function () {
+                                yogurtCaption.style.opacity = "0";
+                                iceCream.style.opacity = "1";
+                                milk.style.opacity = "1";
+                            });
+
+                            milk.addEventListener("mouseover", function () {
+                                milkCaption.style.opacity = "1";
+                                yogurt.style.opacity = "0";
+                                iceCream.style.opacity = "0";
+                            });
+
+                            milk.addEventListener("mouseout", function () {
+                                milkCaption.style.opacity = "0";
+                                yogurt.style.opacity = "1";
+                                iceCream.style.opacity = "1";
+                            });
+                        }, 1000);
+                    }
+                });
+            }
 
         } catch (error) {
             console.log(`Fonts couldn't load: ${error}`);
